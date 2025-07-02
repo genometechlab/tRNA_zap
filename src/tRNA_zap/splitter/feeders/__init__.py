e4df0b672030d7bc7096dd63b2c766206fe77273
PooriaDan <daneshvarkakhaki.p@gmail.com>
2025-07-02 21:43:05 +0000

    Added import for the collate_fn to the feeders module

--- src/tRNA_zap/splitter/feeders/__init__.py
+++ src/tRNA_zap/splitter/feeders/__init__.py
@@ -2,3 +2,4 @@ from .pod5.pod5_mapfeeder import Pod5MapDataset
 from .pod5.pod5_iterfeeder import Pod5IterDataset
 from .sequence_scaler import SequenceScaler
 from .sequence_standardizer import SequenceStandardizer
+from .collate_fn import collate_fn, collate_fn_fixed_padding
