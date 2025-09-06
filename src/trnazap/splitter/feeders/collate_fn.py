15d8d31fadaa29ac65882dbbbb00f18eb0bd4cb4
PooriaDan <daneshvarkakhaki.p@gmail.com>
2025-09-06 00:53:06 +0000

    Inputs to collate-fn is now the batch itself, not the batched appended to list of size 1. Changed the function accordingly

--- src/trnazap/splitter/feeders/collate_fn.py
+++ src/trnazap/splitter/feeders/collate_fn.py
@@ -8,9 +8,6 @@ def collate_fn(batch):
     Alternative implementation using pad_sequence first, then adjusting to fixed length.
     This might be cleaner if you have many sequences much shorter than max_seq_len.
     """
-    assert len(batch) == 1
-    batch = batch[0]
-    
     signals = [torch.tensor(item["inputs"]["signal"]) for item in batch]
     lengths = torch.tensor([item["inputs"]["length"] for item in batch])
     read_ids = [item["metadata"]['read_id'] for item in batch]
