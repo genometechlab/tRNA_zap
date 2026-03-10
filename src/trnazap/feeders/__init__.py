6c938b63f191869f3f3f76a2274bf336dd69b874
pooriadan <daneshvarkakhaki.p@gmail.com>
2026-03-10 16:19:23 +0000

    Removed Pod5IterDataset, since it was for training purposes and wasn't used for inference

--- src/trnazap/feeders/__init__.py
+++ src/trnazap/feeders/__init__.py
@@ -1,4 +1,3 @@
-from .pod5.pod5_iterfeeder import Pod5IterDataset
 from .sequence_scaler import SequenceScaler
 from .sequence_standardizer import SequenceStandardizer
 from .collate_fn import collate_fn
