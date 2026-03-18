4f8dfceb9c36bca85f78dbc0abecfb1c4415474f
Sgakeson <67007663+Sgakeson@users.noreply.github.com>
2026-03-18 16:27:22 -0400

    update label_cli.py

--- src/trnazap/cli/label_cli.py
+++ src/trnazap/cli/label_cli.py
@@ -14,27 +14,36 @@ def register_subparser(subparsers):
     )
     parser.add_argument("--bam", required=True, help="Aligned tRNA file")
     parser.add_argument("--out", required=True, help="Outpath")
-    parser.add_argument("--model", choices = ['yeast', 'ecoli'], required=True, help="Model that labeling is being performed for.")
+    parser.add_argument("--ref", required=False, help="Reference")
+    parser.add_argument("--decoder", required=False, default=None, help="Decoder")
+    parser.add_argument("--model", choices = ['yeast', 'ecoli'], default = None, required=False, help="Model that labeling is being performed for.")
     parser.set_defaults(func=run_label)
 
 
 def run_label(args):
     """Execute the label subcommand."""
-
-    decoder_path = files('trnazap').joinpath('label')
-    if args.model == 'yeast':
-        decoder = decoder_path / "yeast_decoder.pkl"
-    elif args.model == "ecoli":
-        decoder = decoder_path / "ecoli_decoder.pkl"
-
-    ref_path = files('trnazap').joinpath('references')
-    if args.model == 'yeast':
-        ref = ref_path / "label_references" / "sacCer3-mature-tRNAs_biosplint_subset.fa"
-    elif args.model == 'ecoli':
-        ref = ref_path / "label_references" / "eschColi_K_12_MG1655-mature-tRNAs_with_splints.fa"
+    if args.model is not None:
+        decoder_path = files('trnazap').joinpath('label')
+        if args.model == 'yeast':
+            decoder = decoder_path / "yeast_decoder.pkl"
+        elif args.model == "ecoli":
+            decoder = decoder_path / "ecoli_decoder.pkl"
     
-    zap_label(args.bam,
-              ref,
-              args.out,
-              decoder
-              )
+        ref_path = files('trnazap').joinpath('references')
+        if args.model == 'yeast':
+            ref = ref_path / "label_references" / "sacCer3-mature-tRNAs_biosplint_subset.fa"
+        elif args.model == 'ecoli':
+            ref = ref_path / "label_references" / "eschColi_K_12_MG1655-mature-tRNAs_with_splints.fa"
+        
+        zap_label(args.bam,
+                  ref,
+                  args.out,
+                  decoder
+                  )
+
+    else:
+        zap_label(args.bam,
+                  args.ref,
+                  args.out,
+                  args.decoder)
+        
